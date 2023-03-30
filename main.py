114117d3259f023a4eb4313c5dd8ff5ad34f0b88
Евгений <tea-expansion@yandex.ru>
2023-03-30 23:33:52 +0300

    step_four

--- main.py
+++ main.py
@@ -106,7 +106,7 @@ class Board:
             raise BoardOutException()
 
         if d in self.busy:
-            raise BoardUsedException()
+            raise BoardUsedExeption()
 
         self.busy.append(d)
 
@@ -128,6 +128,9 @@ class Board:
         return False
     def begin(self):
         self.busy =[]
+
+    def defeat(self):
+        return  self.count == len(self.ships)
 
 
 class Player:
@@ -175,6 +178,14 @@ class User(Player):
 
 
 class Game:
+    def __init__(self, size = 6):
+        self.size = size
+        pl = self.random_board()
+        co = self.random_board()
+        co.hid = True
+
+        self.ai = AI(co, pl)
+        self.us = User(pl, co)
     def try_board(self):
         lens = [3, 2, 2, 1, 1, 1, 1,]
         board = Board(size = self.size)
@@ -199,8 +210,57 @@ class Game:
             board = self.try_board()
         return board
 
+    def random_board(self):
+        board =None
+        while board is None:
+            board = self.try_board()
+        return  board
+
+    def loop(self):
+        num = 0
+        while True:
+            print("-"*20)
+            print("Доска пользователя:")
+            print(self.us.board)
+            print("-" * 20)
+            print("Доска компьютера:")
+            print(self.ai.board)
+            print("-" * 20)
+            if num % 2 == 0:
+                print("Ходит поьзователь!")
+                repeat = self.us.move()
+            else:
+                print("Ходит компьютер!")
+                repeat = self.ai.move()
+            if repeat:
+                num -= 1
+
+            if self.ai.board.defeat():
+                print("-"*20)
+                print("Пользователь выиграл!")
+                break
+
+            if self.us.board.defeat():
+                print("-"*20)
+                print("Компьютер выиграл!")
+                break
+            num += 1
+
+    def start(self):
+        self.greet()
+        self.loop()
+    def greet(self):
+        print("-------------------")
+        print("  Приветствуем вас ")
+        print("      в игре       ")
+        print("    морской бой    ")
+        print("-------------------")
+        print(" формат ввода: x y ")
+        print(" x - номер строки  ")
+        print(" y - номер столбца ")
+
 g = Game()
-g.size = 6
-print(g.try_board())
+g.start()
+
 
 # 41:19№
